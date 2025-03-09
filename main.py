47d09253fc091df140e3a7cadad33c64ba413db0
Dan Lanfear <lanfeadb@udmercy.edu>
2025-03-09 16:34:47 -0400

    updated to flask- restx and added user class

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask_restx import Resource, Api, fields, marshal_with
 import os
 
 app = Flask(__name__)
 
 api = Api(app)
 
+
+class User:
+    fields = {
+        'name': fields.String,
+        'email': fields.String
+    }
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+
+users = [
+    User(0,"daniel", "ddblanfearjr@yahoo.com", "password"),
+]
+
+
 class Hello(Resource):
     def get(self):
         return jsonify({'message':'hello world'})
-    
 
+@api.route('/users')
 class UserList(Resource):
+    @marshal_with(User.fields)
     def get(self):
-        return jsonify({'message':'users'})
+        return users
 
-
+@api.route('/users/<user_id>')
 class User(Resource):
+    @marshal_with(User.fields)
     def get(self, user_id):
-        return jsonify({'message':'user: ' + user_id})
+        user = users[int(user_id)]
+        return user
 
 
 api.add_resource(Hello, '/')
-api.add_resource(UserList, '/users')
-
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '5000')
