b24bef484ae246a0b240ad393db0b2c8b07ea342
Dan Lanfear <lanfeadb@udmercy.edu>
2025-05-26 15:09:48 -0400

    renamed resources, added transactions and user transactions to database

--- main.py
+++ main.py
@@ -24,20 +24,10 @@ transaction_fields = {
     'date': fields.Date,
     'amount': fields.Fixed(decimals=2),
 }
-
 user_fields = {
         'name': fields.String,
         'email': fields.String,
     } 
-
-users = db.collection('Users')
-
-transactions =[
-    Transaction(0, "kroger", datetime.datetime(2025, 4, 17), 100.00),
-    Transaction(1, "gasoline", datetime.datetime(2025, 4, 18), 50.00),
-    Transaction(2, "wifi", datetime.datetime(2025, 4, 19), 59.00),
-    Transaction(3, "chick fil a", datetime.datetime(2025, 4, 20), 20.00)
-]
 
 cards = []
 
@@ -54,24 +44,26 @@ class Hello(Resource):
     def get(self):
         return jsonify({'message':'hello world'})
 
-@api.route('/users')
-class UserList(Resource):
-    @marshal_with(user_fields)
-    def get(self):
-        try:
-            user_ref = db.collection('Users')
-            users = [doc.to_dict() for doc in user_ref.stream()]
-            return users, 200
-        except Exception as e:
-            return f"An Error Occured: {e}", 400
+# @api.route('/users')
+# class UserList(Resource):
+#     @marshal_with(user_fields)
+#     def get(self):
+#         try:
+#             user_ref = db.collection('Users')
+#             users = [doc.to_dict() for doc in user_ref.stream()]
+#             return users, 200
+#         except Exception as e:
+#             return f"An Error Occured: {e}", 400
 
 @api.route('/users/<user_id>')
-class User(Resource):
+class UserResource(Resource):
     @marshal_with(user_fields)
     def get(self, user_id):
         try:
             user_doc_ref = db.collection("Users").document(user_id)
             user = user_doc_ref.get()
+            if not user.exists:
+                return jsonify({'message':'user not found'}), 404
             return user.to_dict(), 200
         except Exception as e:
             return f"An Error Occured: {e}", 400
@@ -81,19 +73,35 @@ class User(Resource):
     #     user = users[user_id]
     #     # delete user
 
-@api.route('/transactions')
-class TransactionList(Resource):
-    @marshal_with(transaction_fields)
-    def get(self):
-        return transactions
+# @api.route('/transactions')
+# class TransactionList(Resource):
+#     @marshal_with(transaction_fields)
+#     def get(self):
+#         return 
 
 
 @api.route('/transactions/<transaction_id>')
-class Transaction(Resource):
+class TransactionResource(Resource):
     @marshal_with(transaction_fields)
     def get(self, transaction_id):
-        transaction = transactions[int(transaction_id)]
-        return transaction
+        try:
+            transaction_ref = db.collection("transactions").document(transaction_id)
+            transaction = transaction_ref.get()
+            if not transaction.exists:
+                return jsonify({'message':'transaction not found'}), 404
+            return transaction.to_dict(), 200
+        except Exception as e:
+            return f"An Error Occured: {e}", 400
+
+@api.route('/users/<user_id>/transactions')
+class UserTransactionResource(Resource):
+    @marshal_with(transaction_fields)
+    def get(self, user_id):
+        transaction_stream = db.collection('transactions').where('userId', '==', user_id).stream()
+        transaction_list = []
+        for transaction in transaction_stream:
+            transaction_list.append(Transaction.from_dict(transaction.to_dict()).to_dict() | {"id": transaction.id})
+        return transaction_list, 200
 
 
 api.add_resource(Hello, '/')
